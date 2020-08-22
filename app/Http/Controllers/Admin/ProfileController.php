464371dfb6fb0a88f1c1d19ef5222a8f58b2a782
Your Name <you@example.com>
2020-08-22 07:11:14 +0000

    sub_13

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -12,9 +12,23 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
 
-    public function create()
+    public function create(Request $request)
     {
-        return redirect('admin/profile/create');
+        $this->validate($request, News::$rules);
+      $news = new News;
+      $form = $request->all();
+      if (isset($form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $news->image_path = basename($path);
+      } else {
+          $news->image_path = null;
+      }
+      unset($form['_token']);
+      // フォームから送信されてきたimageを削除する
+      unset($form['image']);
+      $news->fill($form);
+      $news->save();
+      return redirect('admin/profile/create');
     }
 
     public function edit()
