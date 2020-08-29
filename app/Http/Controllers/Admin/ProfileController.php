b2f36dd708867f15124afa59ca1f27faf34da9ca
Your Name <you@example.com>
2020-08-29 17:15:52 +0000

    8_29

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -13,31 +13,22 @@ class ProfileController extends Controller
     }
 
     public function create(Request $request)
-    {
-        $this->validate($request, Profile::$rules);
-      $news = new Profile;
-      $form = $request->all();
-      if (isset($form['image'])) {
+   {
+       $this->validate($request, Profile::$rules);
+       
+       $news = new Profile;
+       $form = $request->all();
+       if (isset($form['image'])) {
         $path = $request->file('image')->store('public/image');
         $news->image_path = basename($path);
       } else {
           $news->image_path = null;
       }
       unset($form['_token']);
-      // フォームから送信されてきたimageを削除する
       unset($form['image']);
       $news->fill($form);
       $news->save();
-      return redirect('admin/profile/create');
-    }
-
-    public function edit()
-    {
-        return view('admin.profile.edit');
-    }
-
-    public function update()
-    {
-        return redirect('admin/profile/edit');
+      
+      return redirect('admin/news/create');
     }
 }
