6555ca0c5f6ecd46211e6af83400f4c4decbd70f
Your Name <you@example.com>
2020-08-30 07:33:04 +0000

    8_30_2

--- routes/web.php
+++ routes/web.php
@@ -12,16 +12,16 @@ Route::get('/', function () {
 Route::get('XXX', 'AAAController@bbb');
 
 Route::group(['prefix' => 'admin','middleware' => 'auth'],function (){
-     //dd("流ろにいる");
      Route::get('news/create', 'Admin\NewsController@add');
      Route::post('news/create', 'Admin\NewsController@create');
      Route::get('profile/create','Admin\ProfileController@add');
      Route::post('profile/create','Admin\ProfileController@create');
      Route::get('news', 'Admin\NewsController@index');
-     //Route::get('profile/create','Admin\ProfileController@add');
      Route::get('profile/edit','Admin\ProfileController@edit');
      Route::post('profile/edit','Admin\ProfileController@update');
-     //dd("流ろにいる");
+     Route::get('news/edit', 'Admin\NewsController@edit');
+     Route::post('news/edit', 'Admin\NewsController@update');
+     Route::get('news/delete', 'Admin\NewsController@delete');
 });  
    
     
